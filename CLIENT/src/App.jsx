d217f9078310063cf810b3d31b7717a593a1f1bb
gkartikey05 <gkartikey05@gmail.com>
2024-09-24 22:56:45 +0530

    Added not found page

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./Pages/AboutUs";
 import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
